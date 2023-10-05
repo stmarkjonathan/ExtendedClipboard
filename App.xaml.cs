eb9561b819d56a1212adea408275f6b3bb17c3d1
stmarkj <stmarkjonathan@gmail.com>
2023-10-05 16:34:51 -0400

    added mutex to prevent multiple instances

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +15,22 @@ namespace ExtendedClipboard
     /// </summary>
     public partial class App : Application
     {
+        private Mutex _mutex;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            bool isNewInstance = false;
+            _mutex = new Mutex(true, "ExtendedClipboard", out isNewInstance);
+            
+            if(!isNewInstance )
+            {
+                App.Current.Shutdown();
+            }
+
+            base.OnStartup(e);
+        }
+
     }
+
+
+
 }
